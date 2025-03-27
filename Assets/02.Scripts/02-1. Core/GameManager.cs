abe0cc4f1362a4aee8437883c9ecf5f4783d9a99
Mingyu Choi <88257032+Mingyu0626@users.noreply.github.com>
2025-03-27 09:32:00 +0900

    Add | 필수 에디터 플러그인 및 스크립트 추가

---
+++ Assets/02.Scripts/02-1. Core/GameManager.cs
@@ -0,0 +1,14 @@
+using DG.Tweening;
+using NUnit.Framework;
+using System;
+using System.Security.Cryptography;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GameManager : Singleton<GameManager>
+{
+    protected override void Awake()
+    {
+        base.Awake();
+    }
+}
