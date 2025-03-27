4483a11e2cd83fc7dfa02d240847cae690975183
Mingyu Choi <88257032+Mingyu0626@users.noreply.github.com>
2025-03-27 18:24:55 +0900

    Feat | 게임 내 UI를 담당하는 UI_Game 구현

---
+++ Assets/02.Scripts/02-5. UI/UI_Game.cs
@@ -0,0 +1,72 @@
+using DG.Tweening;
+using System.Collections.Generic;
+using TMPro;
+using UnityEngine.UI;
+using UnityEngine;
+using UnityEngine.SocialPlatforms.Impl;
+
+public class UI_Game : Singleton<UI_Game>
+{
+    [SerializeField] private float _refreshDelay;
+    [SerializeField] private GameObject _hpBarGO;
+    [SerializeField] private GameObject _feverBarGO;
+    private Slider _hpBarSlider;
+    private Slider _feverBarSlider;
+    private Image _hpBarImage;
+    private Image _feverBarImage;
+
+    [SerializeField] private TextMeshProUGUI _comboText;
+    [SerializeField] private TextMeshProUGUI _scoreText;
+
+    protected override void Awake()
+    {
+        base.Awake();
+        _hpBarSlider = _hpBarGO.GetComponent<Slider>();
+        _feverBarSlider = _feverBarGO.GetComponent<Slider>();
+    }
+    private void Start()
+    {
+        InitBarUI(Player.Instance.PlayerData.MaxHealthPoint);
+    }
+    public void InitBarUI(int maxHP)
+    {
+        _hpBarSlider.maxValue = maxHP;
+        _hpBarSlider.value = maxHP;
+        _feverBarSlider.maxValue = 100;
+        _feverBarSlider.value = 0;
+    }
+    public void RefreshBarUI(int hp, int feverGauge)
+    {
+        RefreshBarValue(hp, _hpBarSlider);
+        RefreshBarValue(feverGauge, _feverBarSlider);
+    }
+    private void RefreshBarValue(int hp, Slider slider)
+    {
+        slider.DOValue(hp, _refreshDelay).SetEase(Ease.Linear);
+    }
+    public void SetBarUIEnable(bool val)
+    {
+        _hpBarGO.gameObject.SetActive(val);
+        _feverBarGO.gameObject.SetActive(val);
+    }
+    public void RefreshScore(int score)
+    {
+        _scoreText.text = $"Score : {score:N0}";
+        _scoreText.rectTransform.DOScale(new Vector3(1.4f, 1.4f, 1.4f), 0.08f)
+            .SetEase(Ease.OutBounce)
+            .OnComplete(() =>
+            {
+                _scoreText.rectTransform.localScale = Vector3.one;
+            });
+    }
+    public void RefreshCombo(int combo)
+    {
+        _comboText.text = $"X{combo:N0}";
+        _comboText.rectTransform.DOScale(new Vector3(1.4f, 1.4f, 1.4f), 0.08f)
+            .SetEase(Ease.OutBounce)
+            .OnComplete(() =>
+            {
+                _comboText.rectTransform.localScale = Vector3.one;
+            });
+    }
+}
